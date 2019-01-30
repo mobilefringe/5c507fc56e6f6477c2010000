f45eb886a0d42527c00dfd1227933edb7b04d860
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 18:39:34 -0500

    Langham Square 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -41,21 +41,12 @@
                     currentPage: null
                 }
             },
-            props:['id'],
             beforeRouteUpdate(to, from, next) {
-                host_name = this.property.mm_host.replace("http:", "");
-                this.$store.dispatch('LOAD_PAGE_DATA', {
-                    url: host_name + "/pages/" + to.params.id + ".json"
-                }).then(response => {
-                    this.currentPage = response.data;
-                }, error => {
-                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                    this.$router.replace({ path: '/'});
-                });
+                // this.updateCurrentPage(to.params.id);
                 next();
             },
             created(){
-               this.updateCurrentPage(this.id);
+            //   this.updateCurrentPage(this.id);
             },
             computed: {
                 ...Vuex.mapGetters([
