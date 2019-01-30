6a952335c4b6ff6faeb8b2d5e1eb552ea33c1215
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 17:28:48 -0500

    Langham Square 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -19,7 +19,7 @@
     						</router-link>
     					</span>
     					<div class="text-right  col-sm-6" v-if="promo">
-        					<social-sharing :url="$root.shareURL(promo.slug)" :title="promo.title" :description="promo.body" :quote="_.truncate(promo.description, {'length': 99})" twitter-user="" :media="promo.image_url" inline-template >
+        					<social-sharing :url="$root.shareURL('jobs',promo.slug)" :title="promo.title" :description="promo.body" :quote="_.truncate(promo.description, {'length': 99})" twitter-user="" :media="promo.image_url" inline-template >
                             <div class="blog-social-share">
                                 <div class="social_share">
                                     <network network="facebook">
