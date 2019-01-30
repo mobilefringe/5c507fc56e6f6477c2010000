75e42b9276283c3b45d98ac0f18ea70ab89b30c4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 17:20:16 -0500

    Langham Square 1.0  /main.js

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
     var mixin = {
         data: function() {
             return {
-                dataLoaded: false,
+                site: false,
                 toggle_sitemap: false
             }
         },
