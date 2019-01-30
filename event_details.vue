6bd29057d90fe721ae8ba9b7fe7dfd3e10bfd06f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 16:49:14 -0500

    Langham Square 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -1,18 +1,6 @@
 <template>
 	<div class="event_dets_container page_container" v-if="currentEvent">
-		<div class="row">
-			<div class="col-sm-4 event_logo_container hidden_phone">
-				<div v-if="currentEvent.eventable_type == 'Property'">
-					<img v-if="property.default_logo_url" :src="//codecloud.cdn.speedyrails.net/sites/5beddc9a6e6f6454f6030000/image/jpeg/1544725705000/EventsImage-PlaceHolder-3Thumbnail.jpg" :alt="property.name"/>
-				</div>
-				<div v-else>
-				    <img v-lazy="currentEvent.store.image_url" :alt="urrentEvent.store.name">
-				</div>
-			</div>
-			<div class="col-sm-8 event_image_container text-left">
-				<img v-if="_.includes(currentEvent.image_url, 'missing') != true" :src="currentEvent.image_url" :alt="currentEvent.name"/>
-			</div>
-		</div>
+		
 		<div class="row" style="margin-left:0; margin-top: 20px;">
 			<div class="col-sm-4 event_details_container text-left">
 				<div>
