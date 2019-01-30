a37d6eab55bcfa696f2d61ff1457f366797306ea
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 13:15:56 -0500

    Langham Square 1.0  /plugin.js

--- plugin.js
+++ plugin.js
@@ -1,9 +1,9 @@
-MyPlugin.install = function (Vue, options) {
-  // 1. add global method or property
-  Vue.myGlobalMethod = function () {
-    // some logic ...
-  }
-}
+<!--MyPlugin.install = function (Vue, options) {-->
+<!--  // 1. add global method or property-->
+<!--  Vue.myGlobalMethod = function () {-->
+<!--    // some logic ...-->
+<!--  }-->
+<!--}-->
 export default {
   // The install method will be called with the Vue constructor as
   // the first argument, along with possible options
