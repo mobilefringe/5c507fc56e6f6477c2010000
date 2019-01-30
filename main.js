18c7444188e749a46a54d2e3701c04594c62ac86
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 13:38:54 -0500

    Langham Square 1.0  /main.js

--- main.js
+++ main.js
@@ -51,7 +51,12 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
     Vue.use(Vue2Filters);
     Vue.use(VueI18n);
     Vue.use(SocialSharing);
-    
+    var mixin = {
+      created: function () {
+        console.log('mixin hook called')
+      }
+    }
+
     /* initialize router */
     const router = new VueRouter({
         mode: 'history',
