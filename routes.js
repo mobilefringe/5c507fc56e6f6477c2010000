d83fd9c5bb48b4706ae7c0bc10df0523fc1faf5f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 18:21:45 -0500

    Langham Square 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -25,7 +25,7 @@ define([], function() {
             children: [{
                 path: '',
                 component: view('businesses'),
-                name: 'hourList'
+                name: 'businessesList'
             }]
         },
         {
