276ab8596f7540f822f9144ef0650213e19b80a1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 18:22:01 -0500

    Langham Square 1.0  /businesses.vue

--- businesses.vue
+++ businesses.vue
@@ -33,7 +33,7 @@
 <script>
     define(["Vue", "vuex", "vue!mapplic-svg", "vue-select"], function(Vue, Vuex, MapplicComponent, VueSelect) {
         Vue.component('v-select', VueSelect.VueSelect);
-        return Vue.component("stores-component", {
+        return Vue.component("businessList-component", {
             template: template, // the variable template will be injected
             props:['inside_banner'],
             data: function() {
