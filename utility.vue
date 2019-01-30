96de3aa18ea852a7092fa4135e3164a5da2f5ee6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 13:12:44 -0500

    Langham Square 1.0  /utility.vue

--- utility.vue
+++ utility.vue
@@ -3,25 +3,4 @@ MyPlugin.install = function (Vue, options) {
   Vue.myGlobalMethod = function () {
     // some logic ...
   }
-
-  // 2. add a global asset
-  Vue.directive('my-directive', {
-    bind (el, binding, vnode, oldVnode) {
-      // some logic ...
-    }
-    ...
-  })
-
-  // 3. inject some component options
-  Vue.mixin({
-    created: function () {
-      // some logic ...
-    }
-    ...
-  })
-
-  // 4. add an instance method
-  Vue.prototype.$myMethod = function (methodOptions) {
-    // some logic ...
-  }
 }
