2b327a172085d2707d7915f6056e5c233de8e0ac
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-30 13:40:04 -0500

    Langham Square 1.0  /main.js

--- main.js
+++ main.js
@@ -57,6 +57,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
         },
         methods: {
             shareURL(page,slug){
+                console.log("page, slug", page, slug)
                 var share_url= "http://www.langhamsquare.ca/" + page + "/" + slug;
                 return share_url;
             }
