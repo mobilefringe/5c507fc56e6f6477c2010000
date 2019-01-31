afa2bdc5f79f5a127e96aafa40bd604d32e38991
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-31 15:23:32 -0500

    Langham Square 1.0  /main.js

--- main.js
+++ main.js
@@ -42,6 +42,8 @@ require.config({
         //google map dependcies
         'google-map-api': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCukCjH3fsuDYBdI44hZKL43m60jEToJjY',
         'google_map': 'https://mmvue.codecloudapp.com/google_map.vue?noext',
+        ///google translate
+        "google-translate" : "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit",
     }
 });
 
