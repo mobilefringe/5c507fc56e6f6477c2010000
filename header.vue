cd6f77c179a592a6e2b03133334fee0068f9496b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-31 11:12:48 -0500

    Langham Square 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -119,7 +119,7 @@
 						<ul>
 						    <li v-for="item in menu_items" class="menu_item">
 						        <router-link :to="item.href" class="hvr-underline-from-center" :id="item.id">{{$t(item.name)}}</router-link>
-						        <ul v-if="item.sub_menu">
+						        <ul v-if="item.sub_menu" class="dropdown-content">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
 						                <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</a>
 							            <router-link v-else :to="sub_menu.href" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</router-link>
