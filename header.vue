7aadc76e8ca867a2ae2c5ecbf9d6518e7c2886d3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-01 12:51:18 -0500

    Langham Square 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -62,7 +62,7 @@
 							       
 							        <div v-else role="tablist">
                                         <b-card no-body class="mb-1">
-                                            <b-card-header header-tag="header" class="p-1" role="tab" @click="submenuOpen = !submenuOpen">
+                                            <b-card-header header-tag="header" class="p-1" role="tab" @click="item.show_sub_menu = !submenuOpen">
                                                 <b-btn block href="#" v-b-toggle="$t(item.name)" variant="info">
                                                     {{$t(item.name)}}
                                                     <i v-if="!item.show_sub_menu" class="fa fa-plus"></i>
