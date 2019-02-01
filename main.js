7cb1dbc4cfe840771aa43f4b5d0a77282522639c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-01 13:52:48 -0500

    Langham Square 1.0  /main.js

--- main.js
+++ main.js
@@ -154,7 +154,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                     // footer_sitemap
                     var element = $("#footer_sitemap");
                     console.log("element", $('body')[0].offsetHeight)
-                    var position = $('body')[0].offsetHeight+100;//$("#footer_sitemap").offset().top;
+                    var position = document.body.scrollHeight;//$("#footer_sitemap").offset().top;
                     console.log("position", position)
                     window.scrollTo({
                         top: position,
