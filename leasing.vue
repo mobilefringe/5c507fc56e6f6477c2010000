713ccbc7e02ceabfabcd752eccc857c742a41212
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-20 16:22:35 -0500

    Langham Square 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -97,6 +97,34 @@
                         this.$router.replace({ path: '/'});
                     });
                 },
+                validateBeforeSubmit() {
+                    this.$validator.validateAll().then((result) => {
+                    if (result) {
+                        let errors = this.errors;
+                        send_data = {};
+                        send_data.form_data = JSON.stringify(Utility.serializeObject(this.form_data));
+                        this.$store.dispatch("CONTACT_US", send_data).then(res => {
+                            this.formSuccess = true;
+                        }).catch(error => {
+                            try {
+                                if (error.response.status == 401) {
+                                    console.log("Data load error: " + error.message);
+                                    this.formError = true;
+                                } 
+                                else {
+                                    console.log("Data load error: " + error.message);
+                                    this.formError = true;
+                                }
+                            } 
+                            catch (e) {
+                                console.log("Data load error: " + error.message);
+                                this.formError = true;
+                            }
+                        })
+                        }
+                    
+                    })
+                },
             }
         });
     });
