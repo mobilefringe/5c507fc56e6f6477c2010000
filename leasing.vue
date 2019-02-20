be7712324524c820a186795bef9ef39b180140b1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-20 16:22:08 -0500

    Langham Square 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -5,10 +5,57 @@
 		</div>
         <div class="page_container">
             <div class="row padding_tb_30" >
-            <img src="//codecloud.cdn.speedyrails.net/sites/5c507fc56e6f6477c2010000/image/jpeg/1548064342000/InsidePageImages_940x320px_SpecialtyLeasingPage.jpg" class="" alt="">
-                <div class="page_body description_text text_left padding_tb_30" v-html="currentPage.body">
+                <img src="//codecloud.cdn.speedyrails.net/sites/5c507fc56e6f6477c2010000/image/jpeg/1548064342000/InsidePageImages_940x320px_SpecialtyLeasingPage.jpg" class="" alt="">
+                <div class="page_body description_text text_left padding_tb_30" v-html="currentPage.body"></div>
+                <form class="form-horizontal padding_top_20" action="form-submit" @submit.prevent="validateBeforeSubmit">
+                        <div class="form-group ">
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('name')}">
+                                <label class="label" for="form_name">Name</label>
+                                <input v-model="form_data.name" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="name" type="text" placeholder="Name" data-vv-delay="500" id="form_name">
+                                <span v-show="errors.has('name')" class="form-control-feedback">{{ errors.first('name') }}</span>
+                            </div>
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('email')}">
+                                <label class="label" for="form_email">Email</label>
+                                <input v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" placeholder="Email" data-vv-delay="500" id="form_email">
+                                <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('phone')}">
+                                <label class="label" for="form_phone">Phone</label>
+                                <input v-model="form_data.phone" v-validate="'required|alpha_dash'" class="form-control" :class="{'input': true}" name="phone" type="phone" placeholder="Phone" data-vv-delay="500" id="form_phone">
+                                <span v-show="errors.has('phone')" class="form-control-feedback">{{ errors.first('phone') }}</span>
+                            </div>
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('subject')}">
+                                <label class="label" for="form_subject">Subject</label>
+                                <input v-model="form_data.subject" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="subject" type="text" placeholder="Subject" data-vv-delay="500" id="form_subject">
+                                <span v-show="errors.has('subject')" class="form-control-feedback">{{ errors.first('subject') }}</span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
+                                <label class="label" for="form_message">Message</label>
+                                <textarea v-model="form_data.message" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="message" type="text" placeholder="Message" data-vv-delay="500" id="form_message"></textarea>
+                                <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
+                            </div>
+                        </div>
+                        <div class="form-group account-btn text-left m-t-10">
+                            <div class="col-xs-12">
+                                <button class="feature-readmore hvr-sweep-to-right" type="submit" :disabled="formSuccess">Submit</button>
+                            </div>
+                        </div>
+                    </form>
                     
-                </div>
+                    <div id="send_contact_success" class="alert alert-success text-left" role="alert" v-show="formSuccess">
+                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                        <span class="sr-only">Success</span>
+                        Thank you for contacting us. A member from our team will contact you shortly.
+                    </div>
+                    <div id="send_contact_error" class="alert alert-danger text-left" role="alert" v-show="formError">
+                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                        <span class="sr-only">Error:</span>
+                        There was an error when trying to submit your request. Please try again later.
+                    </div>
             </div>
         </div>
         <div style="padding:20px 0;"></div>
