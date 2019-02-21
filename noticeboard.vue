5fc9f0d5b62a62f00935a113078dbe8dda9c907c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-21 13:37:37 -0500

    Langham Square 1.0  /noticeboard.vue

--- noticeboard.vue
+++ noticeboard.vue
@@ -11,7 +11,7 @@
     				<div class="col-sm-12 promo_div_dets text-left">
     					<p class="promo_div_name">{{promo.title}}</p>
     					<p class="promo_div_date">{{promo.publish_date | moment("MMM D", timezone)}}</p>
-    					<p class="promo_div_description">{{promo.description_short}}</p>
+    					<p class="promo_div_description">{{$root.truncate(promo.body,150)}}</p>
     					<div class="promo_feature_share">
         					<span class="feature_read_more">
         						<router-link :to="'/events/'+promo.slug" class="mobile_readmore" >
