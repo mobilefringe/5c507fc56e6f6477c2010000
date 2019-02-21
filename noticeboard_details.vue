5ecee71bf9c7c62a156af162201cdc09db6baa0f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-21 14:02:43 -0500

    Langham Square 1.0  /noticeboard_details.vue

--- noticeboard_details.vue
+++ noticeboard_details.vue
@@ -90,7 +90,7 @@
                 next();
             },
             created(){
-                this.$store.dispatch("getData", "events").then(response => {
+                this.$store.dispatch("getData", "blogs").then(response => {
                     this.updateCurrentEvent(this.id);
                 }, error => {
                   console.error("Could not retrieve data from server. Please check internet connection and try again.");
