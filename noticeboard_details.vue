16d48181b2c63e2f3c9128ec46a4df2ff82dee2d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-21 14:33:15 -0500

    Langham Square 1.0  /noticeboard_details.vue

--- noticeboard_details.vue
+++ noticeboard_details.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="event_dets_container noticeboard_dets_container page_container" v-if="currentEvent">
-		<div class="row">
+	<div class="event_dets_container noticeboard_dets_container " v-if="currentEvent">
+		<div class="row page_container">
 			<div class="col-sm-12 event_image_container text-left">
 				<!--<img v-if="_.includes(currentEvent.image_url, 'missing') != true" :src="currentEvent.image_url" :alt="currentEvent.name"/>-->
 	 				<div>
