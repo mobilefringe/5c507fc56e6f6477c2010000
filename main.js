da411927e5045a40a9b4ea0b0c9805f461811113
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-26 15:46:09 -0500

    Langham Square 1.0  /main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ require.config({
         'mousewheel' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min',
         'mapplic' : 'https://mmvue.codecloudapp.com/mapplic',
         'hammer' : 'https://codecloud.cdn.speedyrails.net/sites/59bac7db6e6f644f22ba0000/text/javascript/1484859750000/hammer.min',
-        // 'mapplic-svg' : 'https://mmvue.codecloudapp.com/mapplic.vue?noext',
+        'mapplic-svg' : '/mapplic.vue?noext',
         //google map dependcies
         'google-map-api': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCukCjH3fsuDYBdI44hZKL43m60jEToJjY',
         'google_map': 'https://mmvue.codecloudapp.com/google_map.vue?noext',
