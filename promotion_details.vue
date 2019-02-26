0d4f9ad904d11730330f1c8bdbe9bdadaace2f1e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-26 15:52:20 -0500

    Langham Square 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -18,7 +18,7 @@
                     >
                         <h1 class="no_logo_text">
                         {{ currentPromo.store.name }}
-                        <span v-if="promo.store.name_2"><br>({{currentPromo.store.name_2}})</span>
+                        <span v-if="currentPromo.store.name_2"><br>({{currentPromo.store.name_2}})</span>
                           <!--<div class="store_text">-->
                           <!--  <h2></h2>-->
                           <!--</div>-->
