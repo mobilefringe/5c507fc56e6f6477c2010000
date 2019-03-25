f0b38a4359981162af92bbe4e916a4b827a8ba6e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-03-25 15:59:56 -0400

    Langham Square 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -10,7 +10,7 @@
     				</div>
     				<div class="col-sm-5 promo_div_dets text-left">
     					<p class="promo_div_name">{{promo.name}}</p>
-    					<p class="promo_div_date">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
+    					<p class="promo_div_date">{{promo.start_date | moment("MMM D, YYYY", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
     					<p class="promo_div_description">{{promo.description_short}}</p>
     					<div class="promo_feature_share row is-table-row">
         					<span class="feature_read_more col-sm-8">
