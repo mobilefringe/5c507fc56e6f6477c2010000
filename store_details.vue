799611836f014a561d898d8a7e2642a3e01e3e87
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-03-25 15:58:29 -0400

    Langham Square 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -79,7 +79,7 @@
 		        <div class="col-md-5 promo_div_dets">
 		            <p class="promo_div_name">{{promo.name}}</p>
 		            <p class="promo_div_store_name">{{currentStore.name | uppercase}} <span v-if="currentStore.name_2">({{currentStore.name_2}})</span></p>
-		            <p class="promo_div_date">{{promo.start_date | moment("MMM D, YYYY", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
+		            <p class="promo_div_date">{{promo.start_date | moment("MMM D, YYYY", timezone)}} - {{promo.end_date | moment("MMM D, YYYY", timezone)}}</p>
 					<p class="promo_div_description">{{promo.description_short}}</p>
 					<span class="feature_read_more">
 						<router-link :to="'/promotions/'+promo.slug" class="mobile_readmore" >
