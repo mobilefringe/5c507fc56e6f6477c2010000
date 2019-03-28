e4884738165fc3a54a5a54b169915b97aabf0364
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-03-28 13:07:53 -0400

    Langham Square 1.0  /website_support.vue

--- website_support.vue
+++ website_support.vue
@@ -3,7 +3,7 @@
         <div class="margin_25_across padding_top_40" id="contact_us_container">
             <div class="row"> 
                 <div class="col-sm-8 contact_contents">
-                    <img v-if="sideImage" style="margin-bottom:20px;"  :src='sideImage.image_url' alt="">
+                    
                     <div class="description_text text_left">
                         We value your feedback. Send us your comments or questions by completing the contact form below.
                     </div>
