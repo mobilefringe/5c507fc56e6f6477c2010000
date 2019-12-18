e2214d30e1360c5a39b269f884cefa0816af348b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-12-18 16:44:26 -0500

    Langham Square 1.0  /main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ require.config({
         'datastore_mutation_types': 'https://mmvue.codecloudapp.com/datastore_mutation_types',
         'datastore_getters': 'https://mmvue.codecloudapp.com/datastore_getters',
         'datastore_actions': 'https://mmvue.codecloudapp.com/datastore_actions',
-        'datastore_mutations': 'https://mmvue.codecloudapp.com/datastore_mutations',
+        'datastore_mutations': 'https://preview-mmvue.codecloudapp.com/datastore_mutations',
         'datastore': 'https://mmvue.codecloudapp.com/datastore',
         'vue-moment': 'https://mmvue.codecloudapp.com/vue-moment',
         'today_hours': 'https://mmvue.codecloudapp.com/today_hours.vue?noext', //append a dummy query string so requireJS doesn't auto-append .js to the end of the url
