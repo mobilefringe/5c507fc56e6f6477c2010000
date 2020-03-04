3e4bf20fb2a0dd9846a7c0269a24030736bd48f6
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-04 16:07:58 -0500

    Langham Square 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -1,215 +1,133 @@
 define([], function() {
-    return [{
-            path: '/',
-            component: view('home')
-        },
-        {
-            path: '/stores',
-            component: view('default'),
-            children: [{
-                    path: '',
-                    component: view('stores'),
-                    name: 'storeList'
-                },
-                {
-                    path: ':id',
-                    component: view('store_details'),
-                    name: 'storeDetails',
-                    props: true
-                }
-            ]
-        },
-        {
-            path: '/businesses',
-            component: view('default'),
-            children: [{
-                path: '',
-                component: view('businesses'),
-                name: 'businessList'
-            }]
-        },
-        {
-            path: '/restaurants',
-            component: view('default'),
-            children: [{
-                path: '',
-                component: view('restaurants'),
-                name: 'restaurantList'
-            }]
-        },
-        {
-            path: '/promotions',
-            component: view('default'),
-            children: [{
-                    path: '',
-                    component: view('promotions'),
-                    name: 'promoList'
-                },
-                {
-                    path: ':id',
-                    component: view('promotion_details'),
-                    name: 'promotionDetails',
-                    props: true
-
-                }
-            ]
-        },
-        {
-            path: '/events',
-            component: view('default'),
-            children: [{
-                    path: '',
-                    component: view('events'),
-                    name: 'eventList'
-                },
-                {
-                    path: ':id',
-                    component: view('event_details'),
-                    name: 'eventDetails',
-                    props: true
-                }
-            ]
-        },
-        {
-            path: '/jobs',
-            component: view('default'),
-            children: [{
-                    path: '',
-                    component: view('jobs'),
-                    name: 'jobList'
-                },
-                {
-                    path: ':id',
-                    component: view('job_details'),
-                    name: 'jobDetails',
-                    props: true
-                }
-            ]
-        },
-        {
-            path: '/noticeboard',
-            component: view('default'),
-            children: [{
-                    path: '',
-                    component: view('noticeboard'),
-                    name: 'noticeboard'
-                },
-                {
-                    path: ':id',
-                    component: view('noticeboard_details'),
-                    name: 'noticeboardDetails',
-                    props: true
-                }
-            ]
-        },
-        {
-            path: '/hours',
-            component: view('default'),
-            children: [{
-                path: '',
-                component: view('hours'),
-                name: 'hourList'
-            }]
-        },
-        {
-            path: '/directions',
-            component: view('default'),
-            children: [{
-                path: '',
-                component: view('directions'),
-                name: 'directions'
-            }]
-        },
-        {
-            path: '/contact_us',
-            component: view('default'),
-            children: [{
-                path: '',
-                component: view('contact_us'),
-                name: 'contactUs'
-            }]
-        },
-        {
-            path: '/leasing',
-            component: view('default'),
-            children: [{
-                path: '',
-                component: view('leasing'),
-                name: 'leasing'
-            }]
-        },
-        {
-            path: '/newsletter',
-            component: view('default'),
-            children: [{
-                path: '',
-                component: view('newsletter'),
-                name: 'newsletter',
-                props: true
-            }]
-        },
-        {
-            path: '/pages',
-            component: view('default'),
-            redirect: '/404',
-            children: [{
-                path: ':id',
-                component: view('page_details'),
-                name: 'pageDetails',
-                props: true
-            }]
-        },
-        {
-            path: '/website_support',
-            component: view('default'),
-            children: [{
-                path: '',
-                component: view('website_support'),
-                name: 'websiteSupport'
-            }]
-        },
-        {
-            path: '/thank_you',
-            component: view('default'),
-            redirect: '/404',
-            children: [{
-                path: '',
-                component: view('thank_you'),
-                name: 'thankYou'
-            }]
-        },
-        {
-            path: '/search-results',
-            component: view('default'),
-            children: [
-                {
-                    path: '',
-                    component: view('search_results'),
-                    meta: {
-                        pageName: 'Search Results',
-                    },
-                    name: 'search-results'
-                }
-            ]
-        },
-        {
-            path: '/404',
-            name: '404',
-            component: view('notfoundcomponent')
-        },
-        {
-            path: '*',
-            redirect: '/'
+    function e(e) {
+        return function(t) {
+            require(["vue!"+e+".vue"], t)
         }
+    }
+    return[ {
+        path: "/", component: e("home")
+    }
+    , {
+        path:"/stores", component:e("default"), children:[ {
+            path: "", component: e("stores"), name: "storeList"
+        }
+        , {
+            path: ":id", component: e("store_details"), name: "storeDetails", props: !0
+        }
+        ]
+    }
+    , {
+        path:"/businesses", component:e("default"), children:[ {
+            path: "", component: e("businesses"), name: "businessList"
+        }
+        ]
+    }
+    , {
+        path:"/restaurants", component:e("default"), children:[ {
+            path: "", component: e("restaurants"), name: "restaurantList"
+        }
+        ]
+    }
+    , {
+        path:"/promotions", component:e("default"), children:[ {
+            path: "", component: e("promotions"), name: "promoList"
+        }
+        , {
+            path: ":id", component: e("promotion_details"), name: "promotionDetails", props: !0
+        }
+        ]
+    }
+    , {
+        path:"/events", component:e("default"), children:[ {
+            path: "", component: e("events"), name: "eventList"
+        }
+        , {
+            path: ":id", component: e("event_details"), name: "eventDetails", props: !0
+        }
+        ]
+    }
+    , {
+        path:"/jobs", component:e("default"), children:[ {
+            path: "", component: e("jobs"), name: "jobList"
+        }
+        , {
+            path: ":id", component: e("job_details"), name: "jobDetails", props: !0
+        }
+        ]
+    }
+    , {
+        path:"/noticeboard", component:e("default"), children:[ {
+            path: "", component: e("noticeboard"), name: "noticeboard"
+        }
+        , {
+            path: ":id", component: e("noticeboard_details"), name: "noticeboardDetails", props: !0
+        }
+        ]
+    }
+    , {
+        path:"/hours", component:e("default"), children:[ {
+            path: "", component: e("hours"), name: "hourList"
+        }
+        ]
+    }
+    , {
+        path:"/directions", component:e("default"), children:[ {
+            path: "", component: e("directions"), name: "directions"
+        }
+        ]
+    }
+    , {
+        path:"/contact_us", component:e("default"), children:[ {
+            path: "", component: e("contact_us"), name: "contactUs"
+        }
+        ]
+    }
+    , {
+        path:"/leasing", component:e("default"), children:[ {
+            path: "", component: e("leasing"), name: "leasing"
+        }
+        ]
+    }
+    , {
+        path:"/newsletter", component:e("default"), children:[ {
+            path: "", component: e("newsletter"), name: "newsletter", props: !0
+        }
+        ]
+    }
+    , {
+        path:"/pages", component:e("default"), redirect:"/404", children:[ {
+            path: ":id", component: e("page_details"), name: "pageDetails", props: !0
+        }
+        ]
+    }
+    , {
+        path:"/website_support", component:e("default"), children:[ {
+            path: "", component: e("website_support"), name: "websiteSupport"
+        }
+        ]
+    }
+    , {
+        path:"/thank_you", component:e("default"), redirect:"/404", children:[ {
+            path: "", component: e("thank_you"), name: "thankYou"
+        }
+        ]
+    }
+    , {
+        path:"/search-results", component:e("default"), children:[ {
+            path:"", component:e("search_results"), meta: {
+                pageName: "Search Results"
+            }
+            , name:"search-results"
+        }
+        ]
+    }
+    , {
+        path: "/404", name: "404", component: e("notfoundcomponent")
+    }
+    , {
+        path: "*", redirect: "/"
+    }
     ]
+}
 
-    /**
-     * Asynchronously load view (lazy-loading)
-     * @param {string} name the filename (basename) of the view to load.
-     */
-    function view(name) {
-        return function(resolve) {
-            require(['vue!' + name + '.vue'], resolve);
-        }
-    };
-});
+);
