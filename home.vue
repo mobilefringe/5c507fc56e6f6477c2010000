6b6d85017be8f28a7ce424ee09e30897fd112c21
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-05 13:59:28 -0500

    Langham Square 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -81,10 +81,8 @@
         			<div class="feature-div clearfix">
         				<!--<span>-->
         					<div class="feature-box col-sm-4" v-for="item in feature_items">
-        						<!--<p class="feature-title">{{item.name}}</p>-->
-        						<!--<img :src="item.image_url" class="feature-image" :alt="'Click here for more info on '+item.name"/>-->
         						<router-link :to="item.url" class=" feature_link">
-            						<img class="hvr-grow-shadow" :src='item.image_url' :alt="'Click here for more info on ' +item.name"/>
+            						<img class="hvr-grow-shadow" v-lazy="item.image_url" :alt="'Click here for more info on ' +item.name"/>
             						<div class="feature_overlay text-uppercase ">{{item.name}}</div>
         					    </router-link>
         						<!--<p class="feature-html">{{item.html}}</p>-->
