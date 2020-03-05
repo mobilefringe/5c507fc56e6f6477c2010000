7db68a836de273cef2fc2172c623ea8a3578a462
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-05 13:43:13 -0500

    Langham Square 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -59,8 +59,8 @@
         				<slick ref="slick" :options="slickOptions">
         					<div class="" v-for="banner in fiveBanners" v-if="banners">
         						<a v-if="banner.url" :href="banner.url">
-        						    <!--<img :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">-->
-        						    <img v-lazy="img.src" class="hvr-grow-shadow" :alt="banner.name">
+        						    <img :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">
+        						    <!--<img v-lazy="img.src" class="hvr-grow-shadow" :alt="banner.name">-->
         						</a>
         						<img v-else :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">
         					</div>
